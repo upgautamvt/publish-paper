186271b14eb3a2fdb54bac74563870f692f76835
Milo Craun <miloc@vt.edu>
2024-03-25 16:35:29 -0400

    pushing some progress on motivation

---
+++ snippets/s2-codegen-error.c
@@ -0,0 +1,5 @@
+2: (61) r9 = *(u32 *)(r7 +76)
+; R7_w=ctx(id=0,off=0,imm=0) R9_w=pkt(id=0,off=0,r=0,imm=0)
+; return (void *)(unsigned long)ctx->data;
+3: (bc) w6 = w9
+; R6_w=inv(id=0,umax_value=4294967295,var_off=(0x0; 0xffffffff)) R9_w=pkt(id=0,off=0,r=0,imm=0)
