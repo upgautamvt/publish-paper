e7c05f791c9dabd8d4ab6249471f5d381581edbb
Milo Craun <miloc@vt.edu>
2024-03-27 11:37:19 -0400

    More revisions on motivation

---
+++ snippets/s2-refactor.c
@@ -0,0 +1,15 @@
+ static __always_inline int ipv4_ct_extract_l4_ports(...)
+ {
+ #ifdef ENABLE_IPV4_FRAGMENTS
+     ...
+- 	if (unlikely(ipv4_is_fragment(ip4)))
+- 		return ipv4_handle_fragment(...)
+-#endif
++ 	return ipv4_handle_fragmentation(...)
++ #else
+ 	/* load sport + dport into tuple */
+ 	if (ctx_load_bytes(ctx, off, &tuple->dport, 4) < 0)
+ 		return DROP_CT_INVALID_HDR;
++ #endif
+ 	return CTX_ACT_OK;
+ }
---
+++ snippets/s2-switch.c
@@ -0,0 +1,13 @@
+#ifdef ENABLE_SRV6_SRH_ENCAP
+	case NEXTHDR_ROUTING: {
+        ...
+		switch (srh->rthdr.nexthdr) {
+		case IPPROTO_IPIP:
+			goto parse_outer_ipv4;
+		case IPPROTO_IPV6:
+			goto parse_outer_ipv6;
+		default:
+			return DROP_INVALID;
+		}
+	}
+#endif /* ENABLE_SRV6_SRH_ENCAP */
