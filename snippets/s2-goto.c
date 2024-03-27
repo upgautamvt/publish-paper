8ac0c783f8136b096859f211f9893ee68f5657c9
Jinghao Jia <jinghao7@illinois.edu>
2024-03-27 17:12:02 -0500

    pass over s3
    Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

--- snippets/s2-goto.c
+++ snippets/s2-goto.c
@@ -1,14 +1,14 @@
-...
-if (likely(l4policy && !l4policy->wildcard_dport)) {
-  *match_type = POLICY_MATCH_L4_ONLY;
-- policy = l4policy;
-- goto policy_check_entry;
-+ return __account_and_check(ctx, l4policy, ...);
-}
+  ...
+  if (likely(l4policy && !l4policy->wildcard_dport)) {
+    *match_type = POLICY_MATCH_L4_ONLY;
+-   policy = l4policy;
+-   goto policy_check_entry;
++   return __account_and_check(ctx, l4policy, ...);
+  }
 
-if (likely(policy && !policy->wildcard_protocol)) {
-  *match_type = POLICY_MATCH_L3_PROTO;
-- goto policy_check_entry;
-+ return __account_and_check(ctx, policy, ...);
-}
-...
+  if (likely(policy && !policy->wildcard_protocol)) {
+    *match_type = POLICY_MATCH_L3_PROTO;
+-   goto policy_check_entry;
++   return __account_and_check(ctx, policy, ...);
+  }
+  ...
