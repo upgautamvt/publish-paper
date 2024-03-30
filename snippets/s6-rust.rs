74aba8a6d98b053fd47402224855a1763145144f
Ruowen Qin <ruowenq2@illinois.edu>
2024-03-30 11:37:32 +0800

    update indent

--- snippets/s6-rust.rs
+++ snippets/s6-rust.rs
@@ -1,9 +1,9 @@
 let set_iter = payload
-    .windows(4)
-    .enumerate()
-    .filter_map(|(i, v)| 
-      if v == b"set " { Some(i) } else { None }
-    );
+  .windows(4)
+  .enumerate()
+  .filter_map(|(i, v)| 
+    if v == b"set " { Some(i) } else { None }
+  );
 for index in set_iter {
   ...
   payload
