1851589302e173eefdfc6a07e8cb8bd72b107072
Jinghao Jia <jinghao7@illinois.edu>
2024-03-31 15:07:34 -0500

    nits
    Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

--- snippets/s6-bmc.c
+++ snippets/s6-bmc.c
@@ -1,17 +1,17 @@
 #pragma clang loop unroll(disable)
 for (unsigned int off = 0;
-  off < BMC_MAX_PACKET_LENGTH && 
+  off < BMC_MAX_PACKET_LENGTH &&
   payload + off + 1 <= data_end; off++) {
   if (set_found == 0 && payload[off] == 's' &&
-    payload + off + 3 <= data_end && 
-    payload[off + 1] == 'e' &&
-    payload[off + 2] == 't') {
+      payload + off + 3 <= data_end &&
+      payload[off + 1] == 'e' &&
+      payload[off + 2] == 't') {
     ...
-  } else if (key_found == 0 && set_found == 1 && 
+  } else if (key_found == 0 && set_found == 1 &&
              payload[off] != ' ') {
-    if (payload[off] == '\r') { 
+    if (payload[off] == '\r') {
       ...
-    } else { 
+    } else {
       ...
     }
   } else if (key_found == 1) {
