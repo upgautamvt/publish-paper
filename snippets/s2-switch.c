2dcac2ed388d7dede9f164eeb81c4e42c17d50bf
Milo Craun <miloc@vt.edu>
2024-04-01 08:41:30 -0400

    reworded to segment header routing

--- snippets/s2-switch.c
+++ snippets/s2-switch.c
@@ -1,13 +1,21 @@
+switch (ip6->nexthdr) {
 #ifdef ENABLE_SRV6_SRH_ENCAP
-  case NEXTHDR_ROUTING: {
-  ...
-    switch (srh->rthdr.nexthdr) {
-    case IPPROTO_IPIP:
-      goto parse_outer_ipv4;
-    case IPPROTO_IPV6:
-      goto parse_outer_ipv6;
-    default:
-      return DROP_INVALID;
-    }
-  }
+    case NEXTHDR_ROUTING: {
+      ...
+	  switch (srh->rthdr.nexthdr) {
+	  case IPPROTO_IPIP:
+	    goto parse_outer_ipv4;
+	  case IPPROTO_IPV6:
+		goto parse_outer_ipv6;
+	default:
+		return DROP_INVALID;
+	  }
+	}
 #endif /* ENABLE_SRV6_SRH_ENCAP */
+	case IPPROTO_IPIP:
+parse_outer_ipv4: __maybe_unused
+      ...
+	  break;
+	case IPPROTO_IPV6:
+parse_outer_ipv6: __maybe_unused
+      ...
