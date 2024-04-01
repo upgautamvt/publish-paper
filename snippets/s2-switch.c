d01344c62e85c8d331530242864315e27dc29ffd
Jinghao Jia <jinghao7@illinois.edu>
2024-04-01 11:10:47 -0500

    consistent format
    Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

--- snippets/s2-switch.c
+++ snippets/s2-switch.c
@@ -1,21 +1,20 @@
 switch (ip6->nexthdr) {
 #ifdef ENABLE_SRV6_SRH_ENCAP
-    case NEXTHDR_ROUTING: {
-      ...
-	  switch (srh->rthdr.nexthdr) {
-	  case IPPROTO_IPIP:
-	    goto parse_outer_ipv4;
-	  case IPPROTO_IPV6:
-		goto parse_outer_ipv6;
-	default:
-		return DROP_INVALID;
-	  }
-	}
+case NEXTHDR_ROUTING:
+  ...
+  switch (srh->rthdr.nexthdr) {
+  case IPPROTO_IPIP:
+    goto parse_outer_ipv4;
+  case IPPROTO_IPV6:
+    goto parse_outer_ipv6;
+  default:
+    return DROP_INVALID;
+  }
 #endif /* ENABLE_SRV6_SRH_ENCAP */
-	case IPPROTO_IPIP:
+case IPPROTO_IPIP:
 parse_outer_ipv4: __maybe_unused
-      ...
-	  break;
-	case IPPROTO_IPV6:
+  ...
+  break;
+case IPPROTO_IPV6:
 parse_outer_ipv6: __maybe_unused
-      ...
+  ...
