96e18537c49a7242a0e947c5917c188c76783b3c
Jinghao Jia <jinghao7@illinois.edu>
2024-04-02 21:05:23 -0500

    replot mem-footprint
    Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

--- scripts/mem_plot.py
+++ scripts/mem_plot.py
@@ -5,26 +5,26 @@ from matplotlib import rc
 
 rc('font',**{'family':'serif','serif':['Times']})
 rc('text', usetex=True)
-rc('font', size=14)
+# rc('font', size=14)
 
 bpf_programs = {
     'tracex5': [228, 152, 152, 56],
     'trace_event': [700],
     'syscall_tp': [132,132,132,132],
-    'bmc': [524, 864, 984, 1476, 716, 376, 640]
+    'BMC': [524, 864, 984, 1476, 716, 376, 640]
 }
 rust_programs = {
     'tracex5': [0x380, 0x179, 0x68, 0x148],
     'trace_event': [0x0004c8, 0x0001a3, 0x0000d0, 0x000170],
     'syscall_tp': [0x000380,0x000140,0x000048,0x000148],
-    'bmc': [0x000750, 0x001000, 0xc4b, 0x0002a8, 0x000338]
+    'BMC': [0x000750, 0x001000, 0xc4b, 0x0002a8, 0x000338]
 }
 # The adding represents the individual bpf program sizes
 bpf_programs_new = {
     'tracex5': [233 + 160 + 158 + 64],
     'trace_event': [768],
     'syscall_tp': [138 + 138 + 138 + 138],
-    'bmc': [532 + 823 + 1010, 1425 + 667 + 356 + 566]
+    'BMC': [532 + 823 + 1010, 1425 + 667 + 356 + 566]
 }
 page_size = 4096
 
@@ -37,12 +37,12 @@ xticks = []
 def process_one_category(data):
     used = np.array(list(map(sum, data.values())))
     unused = np.array(list(map(lambda x: len(x) * page_size,
-                          bpf_programs.values()))) - used
+                          data.values()))) - used
     return used, unused
 
 data = {
-    'BPF': process_one_category(bpf_programs),
     'BPF-packed': process_one_category(bpf_programs_new),
+    'BPF': process_one_category(bpf_programs),
     'REX': process_one_category(rust_programs)
 }
 
@@ -56,32 +56,40 @@ with plt.style.context('seaborn-v0_8-paper'):
 
     for cat, (used, unused) in data.items():
         offset = width * multiplier
-        rects = ax.bar(x + offset, used, width, label=cat)
-        c = (*rects.patches[0]._facecolor[:-1], 0.3)
-        ax.bar(x + offset, used, width, bottom=used, color=c)
+        rects = ax.bar(x + offset, used, width, label='%s occupied' % cat)
+        # same color but with alpha=0.5
+        c = (*rects.patches[0]._facecolor[:-1], 0.4)
+        ax.bar(x + offset, unused, width, bottom=used, color=c, label='%s empty' % cat)
         multiplier += 1
 
     # Labels and formatting
-    #ax.set_ylabel('Bytes')
-    #ax.set_xticks(xticks)
-    #ax.set_xticklabels(bpf_programs.keys())
+    ax.set_xticks(x + width, x + 1)
+    ax.set_xticklabels(bpf_programs.keys(), size='large')
 
-    # Simplified legend
-    #ax.legend(handles=[
-    #    mpatches.Patch(color='tab:orange', label='BPF Packed'),
-    #    mpatches.Patch(color='tab:blue', label='BPF'),
-    #    mpatches.Patch(color='tab:green', label='REX'),
-    #], loc='upper left')
+    y_min, y_max = ax.get_ylim()
+    y = np.arange(y_min, y_max, 5000)
+    ax.set_yticks(y, list(map(lambda x: '%d' % (x // 1000), y)))
+    ax.set_ylabel('KBs', size='large')
+    ax.set_yticklabels(ax.get_yticklabels(), size='large')
 
-    sec_ax = ax.secondary_yaxis('right', functions=(lambda x: x / page_size, lambda x: x * page_size))
-    sec_ax.set_ylabel('Number of Pages')
+    # Somehow secondary_yaxis does work well with set_yticks and set_yticklabels
+    #sec_ax = ax.secondary_yaxis('right', functions=(lambda x: x / page_size,
+    #                                                lambda x: x * page_size))
+    #sec_ax.set_ylabel('Number of Pages', size='large')
+    #sec_ax.set_ylim(np.array(ax.get_ylim()) / page_size)
+    #sec_ax.set_yticks(sec_ax.get_yticks())
+    #sec_ax.set_yticklabels(sec_ax.get_yticklabels(), size='large')
+
+    sec_ax = ax.twinx()
+    sec_ax.set_ylabel('Number of Pages', size='large')
     sec_ax.set_ylim(ax.get_ylim())
+    sec_ax_yticks = np.arange(*sec_ax.get_ylim(), page_size).astype(np.int64)
+    sec_ax.set_yticks(sec_ax_yticks)
+    sec_ax.set_yticklabels(list(map(str, sec_ax_yticks // page_size)),
+                           size='large')
 
-    # ax.set_xticklabels(ax.get_xticklabels(), size='large')
-    # ax.set_yticklabels(ax.get_yticklabels(), size='large')
-    # sec_ax.set_yticklabels(sec_ax.get_yticklabels(), size='large')
 
-    # ax.legend(loc='upper left', fontsize='small')
+    ax.legend(loc='upper left', ncols=2, fontsize='small')
 
     plt.tight_layout()
     # plt.show()
