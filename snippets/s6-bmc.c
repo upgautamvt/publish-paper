33c4a1aa5e7bb7acdd247dd8a4b237840b7d39c6
Ruowen Qin <chinqrw@gmail.com>
2024-04-05 11:11:44 +0800

    update s6-bmc "conditions"
    Signed-off-by: Ruowen Qin <chinqrw@gmail.com>

--- snippets/s6-bmc.c
+++ snippets/s6-bmc.c
@@ -1,4 +1,3 @@
-int set_found = 0, key_found = 0;
 #pragma clang loop unroll(disable)
 for (unsigned int off = 0;
   off < BMC_MAX_PACKET_LENGTH &&
@@ -8,16 +7,21 @@ for (unsigned int off = 0;
       payload[off + 1] == 'e' &&
       payload[off + 2] == 't') {
     ...
+    set_found = 1;
   } else if (key_found == 0 && set_found == 1 &&
              payload[off] != ' ') {
     if (payload[off] == '\r') {
-      ...
+      set_found = 0;
+      key_found = 0;
     } else {
       ...
+      key_found = 1;
     }
   } else if (key_found == 1) {
     if (payload[off] == ' ') {
       ...
+      set_found = 0;
+      key_found = 0;
     } else {
       ...
     }
