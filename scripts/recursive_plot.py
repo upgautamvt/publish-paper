208de7a19b5ccf207bb5e329e82359c54b880fdc
Jinghao Jia <jinghao7@illinois.edu>
2024-04-08 15:47:14 -0500

    improve recursion plt
    Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

--- scripts/recursive_plot.py
+++ scripts/recursive_plot.py
@@ -1,5 +1,3 @@
-import csv
-
 from matplotlib import rc
 import matplotlib.pyplot as plt
 import numpy as np
@@ -18,12 +16,11 @@ def read_data(folder_path: str, prog_type: str) -> dict:
                 col = []
                 for line in file:
                     col.append(int(line.strip()))
-                all_data[i] = np.average(np.array(col))
+                all_data[i] = np.average(np.array(col[5:])) # dry-run 5 rounds
     return all_data
 
 
 # NOTE: don't change
-
 rc("font", **{"family": "serif", "serif": ["Times"]})
 rc("text", usetex=True)
 
@@ -36,8 +33,8 @@ sorted_rust = [rust_data[key] for key in sorted_keys]
 max_val = max(*sorted_bpf, *sorted_rust)
 
 recursive_results = {
-    "BPF tail call": sorted_bpf,
-    "Rust function call": sorted_rust,
+    "eBPF tail call": (sorted_bpf, '.-'),
+    "REX recursive fn call": (sorted_rust, 's-'),
 }
 
 
@@ -53,31 +50,28 @@ with plt.style.context("seaborn-v0_8-paper"):
     # fig.set_size_inches(8, 4.8) # for slides
     fig.set_size_inches(4.5, 2.7)
 
-    for setup, time_consume in recursive_results.items():
+    # for setup, time_consume in recursive_results.items():
         # offset = width * multiplier
         # NOTE: change to plot
-        rects = ax.plot(
-            sorted_keys,
-            time_consume,
-            label=setup,
-        )
-        print("time_consume")
+    ax.plot(sorted_keys, sorted_bpf, label="eBPF tail call", marker='o', mew=0.1, markersize=4)
+    ax.plot(sorted_keys, sorted_rust, label="REX recursive fn call", marker='^', mew=0.1, markersize=4)
+        # print("time_consume")
         # multiplier += 1
 
+    x = np.arange(sorted_keys[0], sorted_keys[-1], 5).astype(np.int64)
     y_min, y_max = ax.get_ylim()
-    ax.set_ylim(y_min, y_max * 1.12)
-    y = np.arange(y_min, y_max, 0.2)
+    # ax.set_ylim(y_min, y_max * 1.12)
+    y = np.arange(0, y_max + 100, 100).astype(np.int64)
 
     # Add some text for labels, title and custom x-axis tick labels, etc.
     # NOTE: size=large
-    ax.set_xlabel(r"\# of stack depth", size="large")
-    ax.set_ylabel("Run Time (ns)", size="large")
-    # ax.set_xticks(x + width, x + 1)
-    # ax.set_xticklabels(ax.get_xticklabels(), size="large")
-    # ax.set_yticks(y, list(map(lambda x: "%.1f" % x, y)))
-    # ax.set_yticklabels(ax.get_yticklabels(), size="large")
-    ax.legend(loc="upper left", ncols=3, fontsize="small")
+    ax.set_xlabel("Call depth", size="large")
+    ax.set_ylabel("Runtime (ns)", size="large")
+    ax.set_xticks(x)
+    ax.set_xticklabels(ax.get_xticklabels(), size="large")
+    ax.set_yticks(y)
+    ax.set_yticklabels(ax.get_yticklabels(), size="large")
+    ax.legend(loc="upper left", ncols=1, fontsize="small")
 
     # NOTE: don't change
     plt.savefig("./recursive.pdf", bbox_inches="tight")
-    plt.close()
