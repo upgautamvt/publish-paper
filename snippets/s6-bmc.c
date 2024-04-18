fadda5eed7ecacf18dd4b09fdf6c8ad7a8d3af8b
Jinghao Jia <jinghao7@illinois.edu>
2024-04-17 22:20:40 -0500

    squeeze code listings
    Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

--- snippets/s6-bmc.c
+++ snippets/s6-bmc.c
@@ -1,11 +1,10 @@
 #pragma clang loop unroll(disable)
 for (unsigned int off = 0;
-  off < BMC_MAX_PACKET_LENGTH &&
-  payload + off + 1 <= data_end; off++) {
+  off < BMC_MAX_PACKET_LENGTH && payload + off + 1 <= data_end;
+  off++) {
   if (set_found == 0 && payload[off] == 's' &&
       payload + off + 3 <= data_end &&
-      payload[off + 1] == 'e' &&
-      payload[off + 2] == 't') {
+      payload[off + 1] == 'e' && payload[off + 2] == 't') {
     ...
     set_found = 1;
   } else if (key_found == 0 && set_found == 1 &&
