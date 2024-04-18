8e1e0298d407402a8da41df15468ca2e86e7cce3
Jinghao Jia <jinghao7@illinois.edu>
2024-04-18 15:10:55 -0500

    fix code fmt
    Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

--- snippets/s2-kernel-version.c
+++ snippets/s2-kernel-version.c
@@ -1,7 +1,7 @@
 + skb->cb[0] = MARK_MAGIC_TO_PROXY | (proxy_port << 16);
   if (!revalidate_data(skb, &data, &data_end, &ip4))
     return DROP_INVALID;
-  ret = ipv4_l3(skb, ETH_HLEN, (__u8 *) &router_mac, (__u8 *) &host_mac, ip4);
+  ret = ipv4_l3(skb, ...);
   if (IS_ERR(ret))
     return ret;
 - skb->cb[0] = MARK_MAGIC_TO_PROXY | proxy_port << 16;
