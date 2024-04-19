85f2a77eb4479855576a9c4e18bac3a8d715736d
Ruowen Qin <ruowenq2@illinois.edu>
2024-04-19 10:36:09 +0800

    Update recursive.pdf again

--- scripts/recursive_bench/read_data.sh
+++ scripts/recursive_bench/read_data.sh
@@ -2,8 +2,8 @@
 
 for file in *; do
   if [ -f "$file" ] && [ "$file" != "read_data.sh" ]; then # Ensure it's a file
-    sed -i '1,12d' "$file"
     awk '{print $NF}' "$file" > temp_file
     mv temp_file "$file"
+    # sed -i '1,12d' "$file"
   fi
 done
